20bf39b9d1552f46e731dd3ecac9c72ee8902391
Albert <salihbert@yandex.ru>
2017-08-08 16:53:59 +0400

    try button changer color

--- app/src/main/java/space/thealbre/itplace2/MainActivity.java
+++ app/src/main/java/space/thealbre/itplace2/MainActivity.java
@@ -1,9 +1,13 @@
 package space.thealbre.itplace2;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+    public void greenOnClick(View view) {
+        Toast.makeText(this, "You choose the green color", Toast.LENGTH_LONG).show();
+        textZone.setBackgroundColor(getResources().getColor(R.drawable.linea_verde));
+        textZone.setBackgroundColor(Color.CYAN);
+    }
+    public void grayOnClick(View view) {
+        Toast.makeText(this, "You choose the gray color", Toast.LENGTH_LONG).show();
+
+    }
+    public void pinkOnClick(View view) {
+        Toast.makeText(this, "You choose the pink color", Toast.LENGTH_LONG).show();
+
+    }
 
 }
