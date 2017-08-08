d33647b49d43b29c8b3b3aaace32f6e959e8b818
Albert <salihbert@yandex.ru>
2017-08-08 23:22:12 +0400

    final version

--- app/src/main/java/space/thealbre/itplace2/MainActivity.java
+++ app/src/main/java/space/thealbre/itplace2/MainActivity.java
@@ -1,11 +1,13 @@
 package space.thealbre.itplace2;
 
 import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,18 +20,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    private RelativeLayout mConstraintLayout;
+
     public void greenOnClick(View view) {
         Toast.makeText(this, "You choose the green color", Toast.LENGTH_LONG).show();
-        textZone.setBackgroundColor(getResources().getColor(R.drawable.linea_verde));
-        textZone.setBackgroundColor(Color.CYAN);
+        mConstraintLayout = (RelativeLayout) findViewById(R.id.textZone);
+        mConstraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.greenBackground));
     }
+
     public void grayOnClick(View view) {
+        mConstraintLayout = (RelativeLayout) findViewById(R.id.textZone);
+        mConstraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.grayBackground));
         Toast.makeText(this, "You choose the gray color", Toast.LENGTH_LONG).show();
 
     }
     public void pinkOnClick(View view) {
         Toast.makeText(this, "You choose the pink color", Toast.LENGTH_LONG).show();
-
+        mConstraintLayout = (RelativeLayout) findViewById(R.id.textZone);
+        mConstraintLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.pinkBackground));
     }
-
 }
